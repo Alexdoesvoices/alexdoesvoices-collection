20a576fb2c90ba1d128848338f95d3e73048dbaa
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2026-02-07 17:48:07 -0600

    fix maybe

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,9 +4,11 @@ module.exports = {
       name: 'starlight-docs',
       script: 'serve', 
       env: {
-        PM2_SERVE_PATH: './dist', // Relative to the project folder
+        // Use the absolute path to the dist folder inside your project
+        PM2_SERVE_PATH: '/home/arobinson/dev/projects/alexdoesvoices-collection/dist', 
         PM2_SERVE_PORT: 7151,
-        PM2_SERVE_SPA: 'true'
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: '/index.html'
       }
     }
   ]
