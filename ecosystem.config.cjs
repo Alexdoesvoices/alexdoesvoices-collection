399e7292f83b1686066bc828bdd09179fd16caf1
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2026-02-07 17:56:49 -0600

    fix maybe

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: 'starlight-docs',
       script: 'bun',
-      // '-s' is the magic flag that fixes the deep-link (EISDIR) errors
-      args: 'x serve -p 7151 -s dist', 
+      // We force the address to 127.0.0.1 and use the absolute path for the directory
+      args: 'x serve --port 7151 --address 127.0.0.1 /home/arobinson/dev/projects/alexdoesvoices-collection/dist',
       cwd: '/home/arobinson/dev/projects/alexdoesvoices-collection',
       env: {
         NODE_ENV: 'production'
