e0ecd95e3954728a93beaa4e4125369d241451a2
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2026-02-07 14:57:49 -0600

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,22 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Index',
-					link: '/home'
-				},
-				{
-					label: 'Collections',
-					autogenerate: {directory: 'collection'},
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'My Docs',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        sidebar: [
+            {
+                label: 'Index',
+                link: '/home'
+            },
+            {
+                label: 'Collections',
+                autogenerate: {directory: 'collection'},
+            },
+        ],
+		}), mdx()],
 });
