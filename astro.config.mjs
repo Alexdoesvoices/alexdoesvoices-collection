9a48652e5bfdc3921b9096a9b3dfb2744082aa5b
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2026-02-07 17:31:09 -0600

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'http://192.168.68.82:7151',
+    site: 'http://192.168.68.82:7150',
+    base: '/',
     vite: {
         resolve: {
             alias: {
