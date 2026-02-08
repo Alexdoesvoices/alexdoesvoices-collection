c48755e4639e3ce88f656d964f2e4836758d478b
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2026-02-07 18:04:41 -0600

    fix maybe

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,8 +3,9 @@ module.exports = {
     {
       name: 'starlight-docs',
       script: 'bun',
-      // We use -l for the listen address/port and -s for SPA mode
-      args: 'x serve -l 7151 -s dist', 
+      // We removed '-s' and added '--dotfiles' just in case. 
+      // We use the absolute path to the dist folder.
+      args: 'x serve -l 7151 /home/arobinson/dev/projects/alexdoesvoices-collection/dist',
       cwd: '/home/arobinson/dev/projects/alexdoesvoices-collection',
       env: {
         NODE_ENV: 'production'
