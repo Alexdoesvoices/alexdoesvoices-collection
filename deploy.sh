ccb059427e47c64edec4f22d0e0ae72c815522a4
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2026-02-08 15:44:45 -0600

    update

--- deploy.sh
+++ deploy.sh
@@ -18,4 +18,4 @@ echo "Deployment complete! Your app is updated."
 curl -H "Content-Type: application/json" \
      -X POST \
      -d '{"content": "🚀 **Deployment Successful!** AlexDoesVoices-Collection has been updated."}' \
-     https://discord.com/api/webhooks/1470172795651625002/RfuOmrzJeRqHays8MGvdsZYx-5baBpgNFdOA7raNt7qm-IdsUfE2YT5zwjyikUZkL6cF
+    https://discord.com/api/webhooks/1470173396967882897/_CWG0cFP23kr7gQSU9y6Qal6YLFAbeQW1MU5GmTk6c7F7jgtp51ch3hDCkMaWPwxnEq9
