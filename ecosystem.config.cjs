fc3f122ed868c0fcddd818a8e2289149cdb85c22
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2026-03-09 11:24:33 -0500

    test

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,8 @@ module.exports = {
       script: 'bun',
       // We removed '-s' and added '--dotfiles' just in case. 
       // We use the absolute path to the dist folder.
-      args: 'x serve -l 7151 /home/arobinson/dev/projects/alexdoesvoices-collection/dist',
+      // Change this line in your ecosystem.config.cjs:
+      args: 'x serve -l 7151 --host 0.0.0.0 /home/arobinson/dev/projects/alexdoesvoices-collection/dist',      
       cwd: '/home/arobinson/dev/projects/alexdoesvoices-collection',
       env: {
         NODE_ENV: 'production'
